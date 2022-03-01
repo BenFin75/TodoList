a3711ec44bfccdad23ab510ce376e88c03ab700b
Ben Finley <bben10hi@gmail.com>
2022-03-01 16:42:13 -0500

    added logic for task filtering callbacks

--- src/App.js
+++ src/App.js
@@ -7,18 +7,34 @@ import CreateTask from './CreateTask';
 import TaskDetails from './TaskDetails';
 import SignUpPage from './SignUpPage';
 import LogInPage from './LogInPage';
+import { useState } from 'react';
 
 function App() {
+  const [filter, setFilter] = useState('');
+
+  const getFilter = (selection) => {
+    let newFilter;
+    switch (selection) {
+        case 'all':
+        case 'today':
+        case 'week':
+        case 'past-due':
+          setFilter(['date', selection]);
+            break;
+        default:
+          setFilter(['project', selection]);
+    }
+  }
   
   return (
     <Router>
       <div className="App">
         <Navbar />
         <div className="content">
-          <Sidebar/>
+          <Sidebar getFilter={getFilter} />
             <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/create" element={<CreateTask/>} />
+              <Route path="/" element={<Home filter={filter} />} />
+              <Route path="/create" element={<CreateTask />} />
               <Route path="/task/:id" element={<TaskDetails />} />
               <Route path='/users/signup' element={<SignUpPage />} />
               <Route path='/users/login' element={<LogInPage />} />
